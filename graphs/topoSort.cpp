9b9a6024a5ebbe53d81bbbf59da4cd3a88d5e896
BhargaviChanaboina <36034208+BhargaviChanaboina@users.noreply.github.com>
2021-08-18 16:14:12 +0530

    Create topoSort.cpp

---
+++ graphs/topoSort.cpp
@@ -0,0 +1,92 @@
+/*
+Topological sort 
+
+https://practice.geeksforgeeks.org/problems/topological-sort/1
+
+Given a Directed Acyclic Graph (DAG) with V vertices and E edges, Find any Topological Sorting of that Graph.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+	public:
+	
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> res;
+	    vector<int> in(V, 0);
+	    queue<int> q;
+	    for (int i = 0; i < V; i++) {
+	        for (int j = 0; j < adj[i].size(); j++) {
+	            in[adj[i][j]]++;
+	        }
+	    }
+	    for (int k = 0; k < V; k++) {
+	        if (in[k] == 0) q.push(k);
+	    }
+	    
+	    while(!q.empty()) {
+	        int cur = q.front();
+	        q.pop();
+	        res.push_back(cur);
+	        for (int t = 0; t < adj[cur].size(); t++) {
+	            in[adj[cur][t]]--;
+	            if (in[adj[cur][t]] == 0) q.push(adj[cur][t]);
+	        }
+	    }
+	    return res;
+	    
+	}
+};
+
+// { Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}  // } Driver Code Ends
